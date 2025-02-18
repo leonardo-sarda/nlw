e92c010a23a897854a292139bd1c890bff1729fd
leonardo-sarda <leonardo_sarda@hotmail.com>
2025-02-18 17:10:02 -0300

    feito rotas

---
+++ nlw-connect/node/src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,28 @@
+import { z } from "zod"
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
+
+export const subcribeToEventRoute: FastifyPluginAsyncZod = async (app) =>{
+  app.post('/subscriptions', {
+    schema: {
+      body: z.object({
+        name: z.string(),
+        email: z.string().email()
+      }),
+      response:{
+        201: z.object({
+          name: z.string()
+        })
+      }
+    }
+    }, async (request, reply) =>{
+      const { name, email} = request.body
+    
+      return reply.status(201).send({
+        name
+      })
+    })
+    
+    app.get('/dev', () => {
+      return "Dev"
+    })
+}
