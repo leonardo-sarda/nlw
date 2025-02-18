5d281c23f29388ea4719c85f1de6102acb06c8c7
leonardo-sarda <leonardo_sarda@hotmail.com>
2025-02-18 15:46:27 -0300

    feito a rota post com validacao

--- nlw-connect/node/src/server.ts
+++ nlw-connect/node/src/server.ts
@@ -1,17 +1,38 @@
 import {fastify} from 'fastify';  
 import {fastifyCors} from '@fastify/cors'
 import {
-validatorCompiler,
-serializerCompiler
+  validatorCompiler,
+  serializerCompiler,
+  ZodTypeProvider
 } from 'fastify-type-provider-zod'
+import {z} from 'zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors,{
   origin: true,
 })
 
-app.get('/hello', () => {
+app.post('/subscriptions', {
+schema: {
+  body: z.object({
+    name: z.string(),
+    email: z.string().email()
+  })
+}
+}, (request, reply) =>{
+  const { name, email} = request.body
+
+  return reply.status(201).send({
+    name,
+    email
+  })
+})
+
+app.get('/dev', () => {
   return "Dev"
 })
 
