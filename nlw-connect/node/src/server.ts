d6434ee7170700af9313265b38813824c553a65f
leonardo-sarda <leonardo_sarda@hotmail.com>
2025-02-18 17:03:44 -0300

    adicionado o swagger

--- nlw-connect/node/src/server.ts
+++ nlw-connect/node/src/server.ts
@@ -3,9 +3,12 @@ import {fastifyCors} from '@fastify/cors'
 import {
   validatorCompiler,
   serializerCompiler,
-  ZodTypeProvider
+  ZodTypeProvider,
+  jsonSchemaTransform
 } from 'fastify-type-provider-zod'
 import {z} from 'zod'
+import {fastifySwagger} from '@fastify/swagger';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -16,19 +19,37 @@ app.register(fastifyCors,{
   origin: true,
 })
 
+app.register(fastifySwagger,{
+  openapi: {
+     info:{
+      title: 'Nlw connect',
+      version: '0.0.1'
+     }
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi,{
+  routePrefix: '/docs'
+})
+
 app.post('/subscriptions', {
 schema: {
   body: z.object({
     name: z.string(),
     email: z.string().email()
-  })
+  }),
+  response:{
+    201: z.object({
+      name: z.string()
+    })
+  }
 }
-}, (request, reply) =>{
+}, async (request, reply) =>{
   const { name, email} = request.body
 
   return reply.status(201).send({
-    name,
-    email
+    name
   })
 })
 
