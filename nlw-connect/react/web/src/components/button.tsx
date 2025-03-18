61975818aaefca0ef54a823160c4e725656625cd
leonardo-sarda <leonardo_sarda@hotmail.com>
2025-03-18 19:34:00 -0300

    configurado biome

--- nlw-connect/react/web/src/components/button.tsx
+++ nlw-connect/react/web/src/components/button.tsx
@@ -1,4 +1,4 @@
-import type { ComponentProps, ReactNode } from "react"
+import type {  ComponentProps, ReactNode } from "react"
 
 interface ButtonProps extends ComponentProps<'button'>{
   children: ReactNode
