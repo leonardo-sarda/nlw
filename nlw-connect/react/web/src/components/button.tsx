173650349d73a6f4d456b886f89b1bb80b0049a9
leonardo-sarda <leonardo_sarda@hotmail.com>
2025-03-21 16:34:04 -0300

    making pagina de invite

--- nlw-connect/react/web/src/components/button.tsx
+++ nlw-connect/react/web/src/components/button.tsx
@@ -1,16 +1,15 @@
-import type { ComponentProps, ReactNode } from "react"
+import type { ComponentProps, ReactNode } from 'react'
 
-interface ButtonProps extends ComponentProps<'button'>{
+interface ButtonProps extends ComponentProps<'button'> {
   children: ReactNode
-
 }
-export function Button(props: ButtonProps)  {
-  return( 
-  <button  
-    className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900" 
-    {...props}
-  >
-    {props.children}
-  </button>
+export function Button(props: ButtonProps) {
+  return (
+    <button
+      className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      {...props}
+    >
+      {props.children}
+    </button>
   )
 }
